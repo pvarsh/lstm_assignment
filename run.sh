cb5d28d694168b4c16ad9aa72fe7d612fbbb12a6
Peter Varshavsky <pvarsh@gmail.com>
2015-04-27 18:14:42 -0400

    Adds hosted model

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+wget https://dl.dropboxusercontent.com/u/12075991/char_thirteen_epoch_model_seq50.net
 luajit main.lua -seq_length 50 -no_train $true -load $true -load_name char_thirteen_epoch_model_seq50.net -submission $true -char $true
