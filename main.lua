9b8147f8101678f1708b36671083055ec5e1edb7
Peter Varshavsky <pvarsh@gmail.com>
2015-04-27 16:17:18 -0400

    Bug hunting

--- main.lua
+++ main.lua
@@ -358,7 +358,9 @@ function assignment_output()
     print("type input: ", type(input))
     print("type torch.ones()", type(torch.ones(5)))
     print("torch.ones", torch.ones(5))
-    print("buggy line", torch.ones(params.batch_size):mul(input))
+    -- print("buggy line", torch.ones(params.batch_size):mul(input))
+    local x = torch.ones()
+    x:mul(input)
     state_in.data = transfer_data(x)
 
     -- Prepare model and get predictions
